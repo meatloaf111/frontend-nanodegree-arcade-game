64dfba4eef79fb4f15ef2ce94ed979a1c390f53a
meatloaf111 <meatloaf111jp@gmail.com>
2018-09-23 19:51:13 +0900

    Added Player Class

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
+'use strict';
 // Enemies our player must avoid
-var Enemy = function() {
+let Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +18,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +29,55 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+let Player = function(x, y, speed){
+    this.sprite = 'images/char-cat-girl.png';
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+};
+
+Player.prototype.update = function(dt){
+    this.x += this.speed * dt;
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+let allEnemies = [];
+// Init player location
+let player = new Player(0, 0, 50);
+console.log(player.x);
+// Init enemies location
+let enemyInitPosition = [60, 140, 220, 300];
+let enemy;
 
+enemyInitPosition.forEach(function (positionY) {
+    enemy = new Enemy(0, positionY, 50 + Math.floor(Math.random() * 512));
+    allEnemies.push(enemy);
+});
 
+Player.prototype.handleInput = function (keyPress) {
+    switch (keyPress) {
+        case 'left':
+            this.x -= this.speed;
+            break;
+        case 'up':
+            this.y -= this.speed;
+            break;
+        case 'right':
+            this.x += this.speed;
+            break;
+        case 'down':
+            this.y += this.speed;
+            break;
+    }
+};
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
